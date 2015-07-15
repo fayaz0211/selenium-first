1a08708ce03c7e66e4b893060127e466f0ba1a15
fayaz0211 <fayaz0211@gmail.com>
2015-07-15 15:44:40 +0100

    added before n after methods

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -2,24 +2,33 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class SearchTest {
 
 
-	protected  WebDriver driver;
+	private  WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+	}
 	
 	@Test
 	public void searchForItemTest() {
-		
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+	    
 		driver.get("http://www.google.com");
 		System.out.println("the test is ok");
-		driver.quit();
+		
 		
 	}
-
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
